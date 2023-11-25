7a664aa8c127cdef57454e4be4962bdadada39d5
ahed <ahedhamad7@gmail.com>
2023-11-25 20:32:41 +0200

    first commit

--- build.sbt
+++ build.sbt
@@ -1,6 +1,11 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.12"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.5.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.5.0"
+libraryDependencies += "org.apache.logging.log4j" % "log4j-api" % "2.20.0"
+libraryDependencies += "org.apache.logging.log4j" % "log4j-core" % "2.20.0"
+libraryDependencies += "org.scalanlp" %% "breeze" % "1.2"
 
 lazy val root = (project in file("."))
   .settings(
--- src/main/scala/BloomFilterDocuments.scala
+++ src/main/scala/BloomFilterDocuments.scala
@@ -1,9 +1,10 @@
+
+import breeze.numerics.constants.e
 import breeze.util.BloomFilter
 import org.apache.log4j.BasicConfigurator
 import org.apache.log4j.varia.NullAppender
-import org.apache.spark.sql.catalyst.dsl.expressions.longToLiteral
 import org.apache.spark.{SparkConf, SparkContext}
-object BloomFilter {
+object BloomFilterDocuments {
   def main(args: Array[String]): Unit = {
     val nullAppender = new NullAppender
     BasicConfigurator.configure(nullAppender)
@@ -19,9 +20,55 @@ object BloomFilter {
 //     println(f)
 //   })
 val numberOfWords = rddText.count() // m
+  //  println(numberOfWords)
 
-    val bf = rddText.mapPartitions { iter =>
-      val bf = 
+    val bloomFilter = rddText.mapPartitions { iter =>
+      val bloomFilter = BloomFilter.optimallySized[String](numberOfWords, 0.001)
+      iter.foreach(i => bloomFilter += i)
+      Iterator(bloomFilter)
+    }.reduce(_ | _)
+
+    println(bloomFilter.contains("algorithm")) // true
+    println(bloomFilter.contains("Parquet")) // true
+    println(bloomFilter.contains("ahed")) //false
+
+    val rddTextCollect = rddText.collect()
+
+        val resultOfBloomFilter = rddTextCollect.map { word =>
+          val isInBloomFilter= bloomFilter.contains(word)
+          (word,isInBloomFilter)
+        }
+    println("The result of Bloom filter for each  words: ")
+    resultOfBloomFilter.foreach(println)
+
+    val resultOfActualRdd  = rddTextCollect.map { word =>
+      val isInOfActualRdd = rddText.collect().contains(word)
+      (word ,isInOfActualRdd)
+    }
+
+    println("The result of Actual Rdd for each words  : ")
+    resultOfActualRdd.foreach(println)
+
+
+    val comparisonResult = resultOfBloomFilter.zip(resultOfActualRdd).map {
+      case ((wordBf, isInBF), (wordRdd, isInRDD)) =>
+        val isEqual =  isInBF == isInRDD
+        val result = if (isEqual) 1.0 else 0.0
+        (wordBf, result)
+
+    }
+
+    println("The result of comparison between bloom filter and Actual rdd   : ")
+    comparisonResult.foreach(println)
+
+    val totalComparisonResultMatches = comparisonResult.map(_._2).sum
+    val errorRate = 1.0 - (totalComparisonResultMatches / comparisonResult.length)
+
+    println(s"Error Rate is : $errorRate")
+
+    // test data
+    //val ListOfWordsToTest = Seq("ahed", "Parquet", "sensitive", "libraries", "collections", "sources") // Words for testing
+
 
   }
 
